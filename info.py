b74ec864eb0223c3b56966e91102da63301aef17
rohitroy03 <147130159+rohitroy03@users.noreply.github.com>
2023-10-14 14:34:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,8 @@
-API_ID       = 12124605
-API_HASH     = "5cf3577d85fd02286535ec2296934287"
-BOT_TOKEN    = "6083504683:AAEZZAePsjvcKGWOAMNTlaveNl7DD5KZLyo"
-SESSION      = "BQEVDDgAqAeqpwh6ISGOHhApFeAW2aOQ1fgvqNm5kH5dR2DFH3AGfjSAG6gtsq4Mz2Ra6uekSou5X1Breuqjs-DTnHtHNfRVDj3RB86iRFlIF1mA7R4kHDvGF-qVFGMbUjePCSFarxFZch-AHNN4SspYH_i3KHN5h_juhSOSlT5O2iVGIq2CNp22_aPxWkifsBrQnUuyJ3tKTy7_HQ734ICQ5RATZtWtMdO0kCUbY6TyhgYJ0L8jm7AnsqnEmBdM2UkF4fuuasDrOmDocrm39vbITKNgjnucAODx2AtY8bSD5qNDx-BH_vIORzuGF72wqzU19kDzlpyb_-Uqx483blPHyKfOQgAAAAFhSwkGAA"
-DATABASE_URI = "mongodb+srv://MdiskDb:MdiskDb@cluster0.lmirmtn.mongodb.net/?retryWrites=true&w=majority"
-LOG_CHANNEL  = -1001980857896
-ADMIN        = 6051691339
+API_ID       = 27472415  
+API_HASH     = "127886dd538e4ab12be99ec7e08a7142"  
+BOT_TOKEN    = "6488708348:AAFqkCrk6f_JFZvwtYzFESPPkEj3T_TAMoc"  
+SESSION      = "BQGjMh8AKvbbUl-DwMCTuH_NUzm-duyXYdnuUOFmeXp4GUw8Gdte3DTorhjWhlEtvT4Q-gMZvxbajHCaVjM4k3-DGVUBnE7x4l1ExMCb0ycyUrNsH1RQj5f6zWCDUYjU2iTdhBe1y5OYrpE1uUsAaQ2cFof7PZHnOXQm__spnR2R7gYV2ZHbFmgjDvfNTkanE-DslDoiuu7vCu9Z-ntye94iLl-DmNa4ydeW-u0YjxUoER0PZT5F34LLnXWz6Z69x3Nn5ciJqpefqGHii4qeSI4PEZLhGF8N7qhT0Dq7n8R2NCHoXEMU8pzIQP5z1pTRdyetejN3s_P1Mzr5zfqKVmkalK-sBgAAAAFhQogZAA"  
+DATABASE_URI = "mongodb+srv://forward:forward@cluster0.nm08pkc.mongodb.net/?retryWrites=true&w=majority"  
+LOG_CHANNEL  = -1001801752072  
+ADMIN        = 5926717465  
+CHANNEL      = "@sssgggkigfyh"
